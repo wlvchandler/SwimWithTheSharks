9266387b24ba568e4f94e7643ce23396dca1d662
will chandler <jaskamiin@gmail.com>
2025-08-12 00:30:42 -0400

    extend config defaults

--- gossip/config.go
+++ gossip/config.go
@@ -1,8 +1,12 @@
 package gossip
 
 import (
+	crand "crypto/rand"
+	"encoding/hex"
+	"errors"
 	"log/slog"
-	"math/rand"
+	mrand "math/rand"
+	"os"
 	"time"
 )
 
@@ -23,7 +27,7 @@ type Config struct {
 
 	// infra
 	Log   *slog.Logger
-	Rand  *rand.Rand
+	Rand  *mrand.Rand
 	Clock Clock
 
 	// identity (optional. if empty we auto-gen a random one)
@@ -52,4 +56,77 @@ func (c Config) WithDefaults() Config {
 	if out.PingInterval <= 0 {
 		out.PingInterval = 1 * time.Second
 	}
+
+	if out.PingTimeout <= 0 {
+		out.PingTimeout = 200 * time.Millisecond
+	}
+
+	if out.SuspectTimeout <= 0 {
+		out.SuspectTimeout = 5 * time.Second
+	}
+
+	if out.MaxPiggyBack <= 0 {
+		out.MaxPiggyBack = 16
+	}
+
+	if out.IndirectK <= 0 {
+		out.IndirectK = 0
+	}
+
+	if out.Log == nil {
+		out.Log = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	if out.Rand == nil {
+		var seed int64
+		var b [8]byte
+		if _, err := crand.Read(b[:]); err == nil {
+			seed = int64(uint64(b[0]<<56) | uint64(b[1]<<48) | uint64(b[2]<<40) | uint64(b[3]<<32) |
+				uint64(b[4]<<24) | uint64(b[5]<<16) | uint64(b[6]<<8) | uint64(b[7]))
+		} else {
+			seed = time.Now().UnixNano()
+		}
+		out.Rand = mrand.New(mrand.NewSource(seed))
+	}
+	if out.Clock == nil {
+		out.Clock = systemClock{}
+	}
+	if out.ID == "" {
+		out.ID = NewNodeID()
+	}
+	return out
+}
+
+func (c Config) Validate() error {
+	if c.Bind == "" {
+		return errors.New("bind must be set, e.g. ':7946'")
+	}
+	if c.PingInterval <= 0 {
+		return errors.New("PingInterval must be > 0")
+	}
+	if c.PingTimeout <= 0 {
+		return errors.New("PingTimeout must be > 0")
+	}
+	if c.SuspectTimeout <= 0 {
+		return errors.New("SuspectTimeout must be > 0")
+	}
+	if c.PingTimeout >= c.PingInterval {
+		return errors.New("PingTimeout must be < PingInterval")
+	}
+	if c.SuspectTimeout < 3*c.PingInterval {
+		return errors.New("SuspectTimeout should be >= 3 * PingInterval")
+	}
+	if c.MaxPiggyBack <= 0 {
+		return errors.New("MaxPiggyback must be > 0")
+	}
+	if c.IndirectK < 0 {
+		return errors.New("IndirectK must be >= 0")
+	}
+	return nil
+}
+
+func NewNodeID() NodeID {
+	var b [16]byte
+	_, _ = crand.Read(b[:])
+	return NodeID(hex.EncodeToString(b[:]))
 }
