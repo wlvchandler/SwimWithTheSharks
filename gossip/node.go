b9e6075d939d4e204b9feee746a3e7975594f982
will chandler <jaskamiin@gmail.com>
2025-08-12 00:33:50 -0400

    update node type to compile

--- gossip/node.go
+++ gossip/node.go
@@ -2,5 +2,8 @@ package gossip
 
 type Node struct{}
 
-func Start(cfg Config) (*Node, error) { return &Node{}, nil }
-func (n *Node) Close() error          { return nil }
+func Start(cfg Config) (*Node, error) {
+	_ = cfg.WithDefaults()
+	return &Node{}, nil
+}
+func (n *Node) Close() error { return nil }
