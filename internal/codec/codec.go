73749ac72441eb7710e71795e1099f100a25c6f9
will chandler <jaskamiin@gmail.com>
2025-08-12 02:15:41 -0400

    add: json marshal in codec

--- internal/codec/codec.go
+++ internal/codec/codec.go
@@ -3,6 +3,10 @@ package codec
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/wlvchandler/SwimWithTheSharks/gossip"
 )
 
 type jsonRaw = json.RawMessage
@@ -13,4 +17,34 @@ var (
 	ErrDecodeBody  = errors.New("codec: body decode failed")
 )
 
-//TODO: marshal/unmarshal
+func typeOf(b any) (string, error) {
+	switch b.(type) {
+	case Ping, *Ping:
+		return "ping", nil
+	case Ack, *Ack:
+		return "ack", nil
+	case PingReq, *PingReq:
+		return "pingreq", nil
+	case Join, *Join:
+		return "join", nil
+	case JoinAck, *JoinAck:
+		return "joinack", nil
+	case Leave, *Leave:
+		return "leave", nil
+	default:
+		return "", fmt.Errorf("%w: %s", ErrUnknownType, reflect.TypeOf(b))
+	}
+}
+
+func Marshal(from gossip.NodeID, body any) ([]byte, error) {
+	typ, err := typeOf(body)
+	if err != nil {
+		return nil, err
+	}
+	b, err := json.Marshal(body)
+	if err != nil {
+		return nil, err
+	}
+	env := Envelope{Type: typ, From: from, V: Version, Body: b}
+	return json.Marshal(&env)
+}
